a25688731d986b418590094f9a303704ccd26645
Malin Antonsson <malin@malinantonsson.com>
2017-05-29 12:45:37 +0100

    create components file. Create search bar component.

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+//Always need to import react into the scope of the file when using JSX
+import React from 'react';
+
+// define the component class.
+const SearchBar = () => {
+	return <input />
+};
+
+// create a connection (export) to index.js
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const container = document.querySelector('.container');
+const componentsPath = './components/';
+
+import SearchBar from './components/search_bar';
+
+//Youtube API key
+const API_KEY = 'AIzaSyCIZSpV4OHPehNRI8xARiQjWXIeLGedFNc';
+
+const APP_CONTAINER = document.querySelector('.container');
 
 
 // create a new component. This component should produce some HTML.
 // This is a class. to render it, produce an instance of the class. <App></App>
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 
 // take this component's generated HTML and put it on the page (in the DOM)
 // second param is the dom element where the react components is inserted to.
-ReactDOM.render(<App />, container);
+ReactDOM.render(<App />, APP_CONTAINER);
