14d2d18a735a2b0331cb220613e4100896bad5a3
Malin Antonsson <malin@malinantonsson.com>
2017-05-29 13:55:30 +0100

    Add downward data flow & do an initial youtube fetch

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
 //Import and define React using js modules
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
 
 //tried to use template literal to dynamically set the path (so it's DRY)
 //failed because import needs a string literal & cannot be dynamic
 //const componentsPath = './components/';
-
 import SearchBar from './components/search_bar';
 
 //Youtube API key
 const API_KEY = 'AIzaSyCIZSpV4OHPehNRI8xARiQjWXIeLGedFNc';
-
 const APP_CONTAINER = document.querySelector('.container');
 
 
 // create a new component. This component should produce some HTML.
 // This is a class. to render it, produce an instance of the class. <App></App>
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor (props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 
