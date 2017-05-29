9817ab1f127818b5bee35bf1617e905837be2dee
Malin Antonsson <malin@malinantonsson.com>
2017-05-29 12:20:27 +0100

    Start from scratch. Set up & render first component.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+//Import and define React using js modules
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const container = document.querySelector('.container');
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// create a new component. This component should produce some HTML.
+// This is a class. to render it, produce an instance of the class. <App></App>
+const App = () => {
+	return <div>Hi!</div>;
+}
+
+
+// take this component's generated HTML and put it on the page (in the DOM)
+// second param is the dom element where the react components is inserted to.
+ReactDOM.render(<App />, container);
