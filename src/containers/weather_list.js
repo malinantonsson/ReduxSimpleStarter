78dbc1d1f77f4e3e2b6d263bb576ca0ffe9374b5
Malin Antonsson <malin@malinantonsson.com>
2017-06-01 22:45:12 +0100

    add react sparklines and render a graph

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 export class WeatherList extends Component {
-	constructor(props) {
-		super(props);
-
-
-	}
 
 	renderWeather(cityData, index) {
 		const name = cityData.city.name;
-		
+		const temps = cityData.list.map(weather => weather.main.temp);
+		//const temps = [1, 2, 3, 4, 1, 5];
+		console.log(temps);
+
 		return (
 			<tr key={`${name}-${index}`}>
 				<td>{name}</td>
+				<td>
+					<Sparklines data={temps} height={120} width={180}>
+						<SparklinesLine color="red" />
+					</Sparklines>
+				</td>
 			</tr>
 		)
 	}
@@ -22,10 +26,12 @@ export class WeatherList extends Component {
 		return (
 			<table className="table table-hover">
 				<thead>
-					<th>City</th>
-					<th>Temperature</th>
-					<th>Pressure</th>
-					<th>Humidity</th>
+					<tr>
+						<th>City</th>
+						<th>Temperature</th>
+						<th>Pressure</th>
+						<th>Humidity</th>
+					</tr>
 				</thead>
 				<tbody>
 					{this.props.weather.map(this.renderWeather)}
