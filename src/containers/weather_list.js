3984d613189051678fd2af96cb6bdd2dd45f5172
Malin Antonsson <malin@malinantonsson.com>
2017-06-01 22:25:40 +0100

    Map props to render helper & output the data

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class WeatherList extends Component {
+	constructor(props) {
+		super(props);
+
+
+	}
+
+	renderWeather(cityData, index) {
+		const name = cityData.city.name;
+		
+		return (
+			<tr key={`${name}-${index}`}>
+				<td>{name}</td>
+			</tr>
+		)
+	}
+
 	render() {
 		return (
 			<table className="table table-hover">
 				<thead>
-					<tr>City</tr>
-					<tr>Temperature</tr>
-					<tr>Pressure</tr>
-					<tr>Humidity</tr>
+					<th>City</th>
+					<th>Temperature</th>
+					<th>Pressure</th>
+					<th>Humidity</th>
 				</thead>
 				<tbody>
-
-					
+					{this.props.weather.map(this.renderWeather)}
 				</tbody>
 
 			</table>
