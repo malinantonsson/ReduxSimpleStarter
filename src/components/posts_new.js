e0d7479d58be2a119d21e934b23ea85c61b571fd
Malin Antonsson <malin@malinantonsson.com>
2017-06-04 19:10:16 +0100

    create a form with fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+	renderField(field) {
+		return (
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input 
+					type="text"
+					className="form-control"
+					{...field.input}
+				/>
+			</div>
+		)
+	}
+
 	render() {
 		return (
-			<div>
-				PostsNew!
-			</div>
+			<form>
+				<Field
+					label="Title" 
+					name="title"
+					component={this.renderField}
+				/>
+				<Field 
+					label="Tags"
+					name="tags"
+					component={this.renderField}
+				/>
+
+				<Field 
+					label="Post Content"
+					name="Content"
+					component={this.renderField}
+				/>
+
+			</form>
 		);
 	}
 }
 
-export default PostsNew;
+//kind of like the Connect
+//makes it communicate directly to the reducer
+export default reduxForm({
+	form: 'PostsNewForm'
+})(PostsNew);
