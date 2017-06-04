e2172f701382696804650c51c2d3696940471322
Malin Antonsson <malin@malinantonsson.com>
2017-06-04 17:19:07 +0100

    get the data on component render

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { fetchPosts } from '../actions';
 
 class PostIndex extends Component {
+	//called as soon as the component shows up in the DOM
+	componentDidMount() {
+		this.props.fetchPosts();
+	}
+
+	renderPosts() {
+		return _.map(this.props.posts, post => {
+			return (
+				<li className="list-group-item" key={post.id}>
+					{post.title}
+				</li>
+			)
+		});
+	}
+
+
 	render() {
 		return (
 			<div>
-				Post Index
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 			</div>
 		);
 	}
 }
 
-export default PostIndex;
+function mapStateToProps(state) {
+	return { posts: state.posts };
+}
+
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
